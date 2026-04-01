d36375104ea61e73688752ee10adbdaf53496515
h-okabe-beep <h-okabe@kanei.co.jp>
2026-04-01 14:32:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TODO: ここにGASで「ウェブアプリとしてデプロイ」したURLを上書きしてください
 // const GAS_API_URL = "https://script.google.com/macros/s/YOUR_API_ID/exec"; 
-const GAS_API_URL = "https://script.google.com/a/macros/kanei.co.jp/s/AKfycbyevemFE02fPtxkND8tfWLHYQumBZDPuxv3bhQ4QtRos3f9OlwI6Fsdc6gkBnDGshgy9Q/exec"; 
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwQ553BL3TcNjCAcvZ4LiTjguX960VZBzgz5IMn4hAOUHIyU6lVIQkfYnAnBKYQkDh2/exec"; 
 
 // 状態管理
 let currentTasks = [];
